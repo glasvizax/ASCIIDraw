3f7a331ecb19a5c5cca1d05ce9e9e675aae25c34
DESKTOP-DF633JQ\glasvisax <glasvizax@gmail.com>
2026-02-19 14:34:34 +0300

    pushPixel + clearBuffer + draw functions implemented

--- main.cpp
+++ main.cpp
@@ -6,35 +6,93 @@
 struct Framebuffer 
 {
     std::string buffer;
+    size_t size_x;
+    size_t size_y;
 
-};
-
-int main(int argc, char* argv[])
-{
-
-    Framebuffer fb;
-    
-    while (true)
+    void init() 
     {
         CONSOLE_SCREEN_BUFFER_INFO csbi;
         GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
         int columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
         int rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
 
-        static std::string testbuf;
-        testbuf.assign(columns * rows, 'a');
+        size_x = columns;
+        size_y = rows;
 
-        testbuf[0] = 'b';
+        buffer.resize(rows * columns);
+    }
+};
 
-        std::cout << "\x1b[H";
+struct vec2
+{
+    float x;
+    float y;
+};
 
-        for (int y = 0; y < rows; ++y) {
-            std::cout.write(&testbuf[y * columns], columns);
-            std::cout << '\n';
-        }
+struct vec3
+{
+    float x;
+    float y;
+    float z;
+};
 
-        //std::this_thread::sleep_for(std::chrono::milliseconds(50));
+struct vec4
+{
+    float x;
+    float y;
+    float z;
+    float w;
+};
+
+void clearBuffer();
+void pushPixel(char symbol, vec2 pos);
+void draw();
+
+Framebuffer g_main_framebuffer;
+char g_clear_symbol = '-';
+
+int main(int argc, char* argv[])
+{
+    g_main_framebuffer.init();
+    
+    while (true)
+    {
+        clearBuffer();
+
+        pushPixel('b', vec2{ 1, 1 });
+        pushPixel('b', vec2{ 2, 2 });
+        pushPixel('b', vec2{ 3, 3 });
+
+        draw();
     }
 
     return 0;
+}
+
+void clearBuffer()
+{
+    for (int i = 0; i < g_main_framebuffer.size_y; ++i)
+    {
+        for (int j = 0; j < g_main_framebuffer.size_x; ++j)
+        {
+            g_main_framebuffer.buffer[i * g_main_framebuffer.size_x + j] = g_clear_symbol;
+        }
+    }
+}
+
+void pushPixel(char symbol, vec2 pos)
+{
+    size_t y = g_main_framebuffer.size_y - 1 - pos.y;
+    size_t idx = y * g_main_framebuffer.size_x + pos.x;
+    g_main_framebuffer.buffer[idx] = symbol;
+}
+
+void draw()
+{
+    std::cout << "\x1b[H";
+
+    for (int y = 0; y < g_main_framebuffer.size_y; ++y) 
+    {
+        std::cout.write(&g_main_framebuffer.buffer[y * g_main_framebuffer.size_x], g_main_framebuffer.size_x);
+    }
 }
