dd70abab06cb4d9552ab31181c5a934beafe33c2
DESKTOP-DF633JQ\glasvisax <glasvizax@gmail.com>
2026-02-21 21:53:58 +0300

    window class start + pushLine refine

--- main.cpp
+++ main.cpp
@@ -29,6 +29,38 @@ struct Framebuffer
     }
 };
 
+struct Window 
+{
+    void init() 
+    {
+        HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+        COORD newSize;
+        newSize.X = 100;
+        newSize.Y = 100;
+        SetConsoleScreenBufferSize(hOut, newSize);
+
+        SMALL_RECT r;
+        r.Left = 0;
+        r.Top = 0;
+        r.Right = newSize.X - 1;
+        r.Bottom = newSize.Y - 1;
+        SetConsoleWindowInfo(hOut, TRUE, &r);
+
+        CONSOLE_FONT_INFOEX cfi = {};
+        cfi.cbSize = sizeof(cfi);
+        GetCurrentConsoleFontEx(hOut, FALSE, &cfi);
+
+        cfi.dwFontSize.X = 8;
+        cfi.dwFontSize.Y = 9;
+        wcscpy_s(cfi.FaceName, L"Terminal");
+
+        SetCurrentConsoleFontEx(hOut, FALSE, &cfi);
+    }
+
+};
+
+
 void clearBuffer();
 void pushPixel(char symbol, xm::uvec2 pos);
 void pushLine(char symbol, xm::ivec2 a, xm::ivec2 b);
@@ -42,29 +74,27 @@ void swap(xm::vector<N, T>& a, xm::vector<N, T>& b)
     b = tmp;
 }
 
+Window g_main_window;
 Framebuffer g_main_framebuffer;
-char g_clear_symbol = '-';
+char g_clear_symbol = '#';
 
 int main(int argc, char* argv[])
 {
+    g_main_window.init();
     g_main_framebuffer.init();
     
     while (true)
     {
         clearBuffer();
-        xm::ivec2 a(7, 3);
-        xm::ivec2 b(12, 37);
-        xm::ivec2 c(62, 53);
+        xm::ivec2 a(0, 0);
+        xm::ivec2 b(50, 50);
+        xm::ivec2 c(50, 0);
 
-        int ax = 7, ay = 3;
-        int bx = 12, by = 37;
-        int cx = 62, cy = 53;
+        pushLine('.', c, b);
+        pushLine('.', a, b);
 
-        pushLine('b', a, b);
-        pushLine('g', c, b);
-
-        pushLine('y', c, a);
-        pushLine('r', a, c);
+        pushLine('.', c, a);
+        pushLine('.', a, c);
         draw();
     }
 
@@ -95,7 +125,7 @@ void pushPixel(char symbol, xm::uvec2 pos)
 void pushLine(char symbol, xm::ivec2 a, xm::ivec2 b)
 {
     bool steep = std::abs(a.x - b.x) < std::abs(a.y - b.y);
-    if (steep) { // if the line is steep, we transpose the image
+    if (steep) { 
         std::swap(a.x, a.y);
         std::swap(b.x, b.y);
     }
@@ -105,27 +135,25 @@ void pushLine(char symbol, xm::ivec2 a, xm::ivec2 b)
         swap(a, b);
     }
 
+    int y = a.y;
+    float error = 0;
+    int ierror = 0;
     for(uint x = a.x; x <= b.x; ++x)
     {
-        float t = (x - a.x) / static_cast<float>((b.x - a.x));
-
-        xm::uvec2 curr;
-        curr.x = x;
-        curr.y = a.y + (b.y - a.y) * t;
-
-        if (steep) // if transposed, de−transpose
-            pushPixel(symbol, xm::uvec2(curr.y, curr.x));
+        if (steep)
+        {
+            pushPixel(symbol, xm::uvec2(y, x));
+        }
         else
-            pushPixel(symbol, xm::uvec2(curr));
+        {
+            pushPixel(symbol, xm::uvec2(x, y));
+        }
+        
+        ierror += 2 * std::abs(b.y - a.y);
+        y += b.y > a.y ? 1 : -1 * (ierror > (b.x - a.x));
+        ierror -= 2 * (b.x - a.x) * (ierror > (b.x - a.x));
     }
-    
-    /*
-    for(float t = 0.0f; t <= 1.0f; t += 0.05f)
-    {
-        xm::uvec2 curr = a + (b - a) * t;
-        pushPixel(symbol, curr);
-    }
-    */
+
 }
 
 void draw()
